5aa712351922235c67348fe12eab89f7748ec5fe
nicolebid <nrbidwell@gmail.com>
2024-09-22 14:16:52 -0700

    ensemble models

--- src/config.py
+++ src/config.py
@@ -2,7 +2,13 @@
 import os 
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 RAW_DATA = os.path.join(ROOT_DIR, 'data', 'AB_NYC_2019.csv')
+FEAT_ENG_DATA = os.path.join(ROOT_DIR, 'data', 'output', 'feature_engineered.csv')
+X_TRAIN_DATA = os.path.join(ROOT_DIR, 'data', 'output', 'X_train.csv')
+Y_TRAIN_DATA = os.path.join(ROOT_DIR, 'data', 'output', 'y_train.csv')
+
 IMG_OUTPUT_DIR = os.path.join(ROOT_DIR, 'output', 'img')
 DATA_OUTPUT_DIR = os.path.join(ROOT_DIR, 'data', 'output')
-FEAT_ENG_DATA = os.path.join(ROOT_DIR, 'data', 'output', 'feature_engineered.csv')
+MODEL_OUTPUT_DIR = os.path.join(ROOT_DIR, 'output', 'models')
+RESULTS_OUTPUT_DIR = os.path.join(ROOT_DIR, 'output', 'results')
---
+++ src/models/ensemble_models.py
@@ -0,0 +1,72 @@
+import pandas as pd 
+import joblib
+import os
+from .. import config
+from sklearn.model_selection import cross_validate
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+
+from ..preprocessor import create_preprocessor
+
+def main(RESULTS_OUTPUT):
+    """
+    Main function to create the ensemble models. 
+    
+    Parameters
+    ----------   
+    RESULTS_OUTPUT : str
+        The path to the directory for saving the cross-validation results. 
+    """
+    # Check necessary data exists
+    if not os.path.isfile(config.X_TRAIN_DATA):
+        print(f"Error: The X_train data does not exist.")
+        return  # exit function
+    
+    if not os.path.isfile(config.Y_TRAIN_DATA):
+        print(f"Error: The y_train data does not exist.")
+        return  # exit function
+    
+    # Load data
+    X_train = pd.read_csv(config.X_TRAIN_DATA)
+    y_train = pd.read_csv(config.Y_TRAIN_DATA)
+    y_train = y_train.values.ravel()
+    
+    preprocessor = create_preprocessor()
+
+    #Train RF 
+    pipe_rf = make_pipeline(
+    preprocessor, RandomForestRegressor(random_state=123, max_depth=10))
+
+    cv_rf = pd.DataFrame(cross_validate(pipe_rf, X_train, y_train, cv=10, return_train_score=True))
+
+    # Train XGboost 
+    pipe_xgb = make_pipeline(preprocessor, XGBRegressor(
+                    random_state=123, verbosity=0, max_depth=3, gamma=3, learning_rate=0.3))
+    cv_xgb = pd.DataFrame(cross_validate(pipe_xgb, X_train, y_train, cv = 10, return_train_score = True))
+
+    # Train LGBM
+    pipe_lgbm = make_pipeline(preprocessor, LGBMRegressor(random_state=123, verbosity = 0))
+    cv_lgbm = pd.DataFrame(cross_validate(pipe_lgbm, X_train, y_train, cv = 10, return_train_score = True))
+
+    # Save results
+    cv_results = {'randomforest': cv_rf.agg(['mean', 'std']).round(3).T}
+    print('RF:', cv_results)
+    output_file = os.path.join(RESULTS_OUTPUT, 'cv_results_rf.joblib') 
+    joblib.dump(cv_results, output_file)
+
+    cv_results = {'XGBoost': cv_xgb.agg(['mean', 'std']).round(3).T}
+    print('XGB:', cv_results)
+    output_file = os.path.join(RESULTS_OUTPUT, 'cv_results_xgb.joblib') 
+    joblib.dump(cv_results, output_file)
+
+    cv_results = {'LGBMRegressor': cv_lgbm.agg(['mean', 'std']).round(3).T}
+    print('LGBM:', cv_results)
+    output_file = os.path.join(RESULTS_OUTPUT, 'cv_results_lgbm.joblib') 
+    joblib.dump(cv_results, output_file)
+
+if __name__ == "__main__":
+    RESULTS_OUTPUT = config.RESULTS_OUTPUT_DIR
+    os.makedirs(RESULTS_OUTPUT, exist_ok=True)
+    main(RESULTS_OUTPUT)
